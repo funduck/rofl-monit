61506c5a96b579c5aeb6cbd07add24b5aecc9281
funduck <qlfunduck@gmail.com>
2022-09-30 16:55:15 +0300

    domain aggregate representation

--- src/domain/core/aggregates/representation.ts
+++ src/domain/core/aggregates/representation.ts
@@ -1,8 +1,37 @@
-import { DomainEntity } from "../entity";
+import { Observable } from "../entities/observable";
+import { DomainEntity, DomainEntityId } from "../entity";
+import { RepresentationState, RepresentationStateUnknown } from "../values/representation_state";
 
+/**
+ * Respresentation of an observable object.
+ */
 export class Representation extends DomainEntity {
-    constructor(
-        private observableId: string,
 
-    ) {}
+    protected statesHistory: Array<RepresentationState> = []
+
+    constructor(public readonly observableId: DomainEntityId<Observable>) {
+        super()
+    }
+
+    public state(): RepresentationState {
+        return this.statesHistory.at(-1) || RepresentationStateUnknown
+    }
+
+    public previousState(): RepresentationState {
+        return this.statesHistory.at(-2) || RepresentationStateUnknown
+    }
+
+    /**
+     * Change state
+     */
+    public transition(state: RepresentationState): void {
+        this.statesHistory.push(state)
+    }
+
+    /**
+     * Reset to initial state.
+     */
+    public reset(): void {
+        this.statesHistory = []
+    }
 }
--- src/domain/core/values/representation_state.ts
+++ src/domain/core/values/representation_state.ts
@@ -1,3 +1,9 @@
 import { DomainValue } from "../value";
 
-export class RepresentationState extends DomainValue {}
+export class RepresentationState extends DomainValue {
+    constructor(descr: string = '') {
+        super(descr)
+    }
+}
+
+export const RepresentationStateUnknown = new RepresentationState('unknown')
