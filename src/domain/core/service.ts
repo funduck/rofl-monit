eb72a933eb7f5fcaf1b31c694f6cb17e5218beb7
funduck <qlfunduck@gmail.com>
2022-09-30 16:53:09 +0300

    docstring to domain service

--- src/domain/core/service.ts
+++ src/domain/core/service.ts
@@ -1,6 +1,12 @@
 import { DomainEvent, DomainEventPublisher, DomainEventSubscriber } from "./event";
 
+/**
+ * Base class for domain services.
+ * One service should perform one operation.
+ * Application may subscribe it for events.
+ */
 export abstract class DomainService implements DomainEventSubscriber {
+
     constructor(protected eventPublisher: DomainEventPublisher) {}
 
     abstract handleEvent(event: DomainEvent): void;
