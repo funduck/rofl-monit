8aad65da1e7413703d35b84aaa3139f4d50fc23e
funduck <qlfunduck@gmail.com>
2022-10-03 17:09:04 +0300

    vscode debugger + typescript + jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "src/.*test\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
